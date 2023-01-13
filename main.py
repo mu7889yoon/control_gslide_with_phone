db64310acbbcd9e16ac17b364f3fcf9a41aa68db
Y.Nakamura <ny16274main@outlook.jp>
2023-01-13 10:18:16 +0900

    python3 main.pyでサーバが起動するように修正

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import conf
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pyautogui
+import os
+import setup
 
 app = FastAPI()
 
 origins = [
     "http://localhost",
-    conf.phone_ip,
 ]
 
 app.add_middleware(
@@ -18,15 +18,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+    
 @app.get("/next")
 async def next():
     pyautogui.press('right')
-    print("next")
     return {"message": "Next"}
 
 @app.get("/prev")
 async def prev():
     pyautogui.press('left')
-    print("prev")
     return {"message": "Prev"}
+
+
+if __name__ == "__main__":
+    setup.main()
+    import conf
+    uvicorn.run(app, host=conf.computer_ip)
